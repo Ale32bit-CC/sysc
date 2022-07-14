ffbea429e95ae25588796eb8c3e59586a4ec32d7
Alessandro Proto <me@alexdevs.me>
2022-07-14 22:21:07 +0200

    reload

--- sysc/sysc.lua
+++ sysc/sysc.lua
@@ -1,5 +1,3 @@
-local lip = require("libs.lip")
-
 local services = {}
 
 local function resolveName(name)
@@ -12,9 +10,19 @@ end
 
 local function getService(name)
     if fs.exists("sysc/services/" + name) then
-        return lip.load("sysc/services/" + name)
+        local func, err = loadfile("sysc/services/" + name)
+        if not func then
+            return nil, err
+        end
+
+        local ok, par = pcall(func)
+        if not ok then
+            return nil, par
+        end
+
+        return par
     end
-    return nil
+    return nil, "service not found"
 end
 
 local function start(name)
@@ -29,19 +37,47 @@ local function kill(name)
 
 end
 
+local function reload(name)
+
+end
+
 local function status(name)
 
 end
 
 local function enable(name)
-    if services[name] then
-        
+    if not services[name] then
+        return
     end
 end
 
 local function disable(name)
-
+    if services[name] then
+        return
+    end
 end
+
+local function daemonReload()
+    local enabledServices = {}
+    if fs.exists("sysc/enabled") then
+        local f = fs.open("sysc/enabled", "r")
+        enabledServices = textutils.unserialise(f.readAll())
+        f.close()
+    end
+
+    for k, v in ipairs(enabledServices) do
+        local name = resolveName(v)
+        local path = resolvePathName(name)
+        local service, err = getService(name)
+        if service then
+            if not services[name] then
+                services[name] = service
+            end
+        end
+    end
+end
+
+daemonReload()
 
 while true do
     local _, id, command, name = os.pullEventRaw("sysc")
